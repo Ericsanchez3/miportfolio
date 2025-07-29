bc642d16d64a3186f0a936f31af65997f6d2967d
EricSanchez3 <slahcini.es@gmail.com>
2025-07-29 20:26:02 +0200

    Cambio de color en la web

--- js/particles.js
+++ js/particles.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     }
                 },
                 "color": {
-                    "value": "#3498db"
+                    value: "#4CAF50" // Verde
                 },
                 "shape": {
                     "type": "circle",
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 "line_linked": {
                     "enable": true,
                     "distance": 150,
-                    "color": "#3498db",
+                    color: "#2E7D32", // Verde oscuro
                     "opacity": 0.4,
                     "width": 1
                 },
